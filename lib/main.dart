f648defe0e20be4b742f745c59527aa971f86c16
johnnylyimo <johnnylyimo@gmail.com>
2023-08-15 05:01:00 +0300

    Defined ThemeData's inversePrimary color

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.deepPurple,
+            inversePrimary: const Color(0xff14b8a6)),
         useMaterial3: true,
       ),
       home: const Home(),
