003a22f270b96aa5a80d3919c6b5969a4f12072f
johnnylyimo <johnnylyimo@gmail.com>
2023-08-17 19:18:37 +0300

    Wrapped Padding widget with BackdropFilter widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -34,42 +36,46 @@ class Home extends StatelessWidget {
           children: [
             SizedBox(
               width: MediaQuery.of(context).size.width / 1.3,
-              child: const Card(
-                color: Color(0xff99F6EC),
+              child: Card(
+                color: const Color(0xff99F6EC),
                 elevation: 10,
-                shape: RoundedRectangleBorder(
+                shape: const RoundedRectangleBorder(
                   borderRadius: BorderRadius.all(Radius.circular(30)),
                 ),
-                child: Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        "300 °F",
-                        style: TextStyle(
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold,
+                child: BackdropFilter(
+             
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "300 °F",
+                          style: TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Icon(
-                        Icons.cloud,
-                        color: Colors.white,
-                        size: 68,
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        'Rain',
-                        style: TextStyle(
-                          fontSize: 22,
+                        SizedBox(
+                          height: 5,
                         ),
-                      )
-                    ],
+                        Icon(
+                          Icons.cloud,
+                          color: Colors.white,
+                          size: 68,
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          'Rain',
+                          style: TextStyle(
+                            fontSize: 22,
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
