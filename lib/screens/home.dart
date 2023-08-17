83b5517805f4494c5460ee6aff10ddacf162470c
johnnylyimo <johnnylyimo@gmail.com>
2023-08-17 20:50:47 +0300

    Extract Card widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -101,49 +101,60 @@ class Home extends StatelessWidget {
             const SizedBox(
               height: 5,
             ),
-            Row(
+            const Row(
               children: [
-                Card(
-                  elevation: 6.0,
-                  color: const Color(0xff99F6EC),
-                  child: Container(
-                    width: 100,
-                    height: 100,
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 8.0),
-                    child: const Column(
-                      children: [
-                        Text(
-                          '03:00',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Icon(
-                          Icons.cloud,
-                          size: 30,
-                          color: Colors.white,
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text(
-                          '120.23',
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                )
+                ForecastCard(),
+                
               ],
             )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ForecastCard extends StatelessWidget {
+  const ForecastCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6.0,
+      color: const Color(0xff99F6EC),
+      child: Container(
+        width: 100,
+        height: 100,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8.0),
+        child: const Column(
+          children: [
+            Text(
+              '03:00',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Icon(
+              Icons.cloud,
+              size: 30,
+              color: Colors.white,
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              '120.23',
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
           ],
         ),
       ),
