808f3f6a6c33a4cc17c45b870499367ee1f3cb79
johnnylyimo <johnnylyimo@gmail.com>
2023-08-22 18:59:43 +0300

    Passed parameters to HourlyForecastItem widget 4

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -123,7 +123,11 @@ class Home extends StatelessWidget {
                     icon: Icons.cloud,
                     temperature: '301.22',
                   ),
-                  HourlyForecastItem(),
+                  HourlyForecastItem(
+                    time: '06:00',
+                    icon: Icons.sunny,
+                    temperature: '249.22',
+                  ),
                   HourlyForecastItem(),
                   HourlyForecastItem(),
                   HourlyForecastItem(),
