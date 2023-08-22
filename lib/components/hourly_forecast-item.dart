1335b2c3623cbf28f08d709cddcbcc3578ecaea8
johnnylyimo <johnnylyimo@gmail.com>
2023-08-22 17:36:57 +0300

    Created new StatelessWidget called HourlyForecastItem

--- lib/components/hourly_forecast-item.dart
+++ lib/components/hourly_forecast-item.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+
+class HourlyForecastItem extends StatelessWidget {
+  const HourlyForecastItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
