ee7894dd10faff73f8e9c816ea7543ac95fa5fb8
johnnylyimo <johnnylyimo@gmail.com>
2023-08-22 17:42:01 +0300

    Moved widget to components folder

---
+++ lib/components/hourly_forecast_item.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class HourlyForecastItem extends StatelessWidget {
+  const HourlyForecastItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6.0,
+      color: const Color(0xff99F6EC),
+      child: Container(
+        width: 100,
+        height: 100,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8.0),
+        child: const Column(
+          children: [
+            Text(
+              '03:00',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Icon(
+              Icons.cloud,
+              size: 30,
+              color: Colors.white,
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              '120.23',
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
