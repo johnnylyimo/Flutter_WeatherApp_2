2bce642f13dc71499da8f8575cb2b1be3b48ae57
johnnylyimo <johnnylyimo@gmail.com>
2023-08-22 16:51:31 +0300

    Added Text widget to the Column's children

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -143,7 +143,8 @@ class Home extends StatelessWidget {
                     ),
                     SizedBox(
                       height: 8,
-                    )
+                    ),
+                    Text()
                   ],
                 )
               ],
